a3ee8487c8e7c60ae1545a7c0468ddb496fdc8c4
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-12 15:17:59 +0900

    Add update_board function and refactor canPut function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,40 @@ const discColor = (num: number): string => {
   }
 };
 
+function update_board(x: number, y: number, range: number[][], board: number[][]): number[][] {
+  const current_color = board[y][x];
+
+  for (const [dx, dy] of range) {
+    //上から下の場合
+    if (x === dx && y > dy) {
+      // eslint-disable-next-line for-direction
+      for (let i = y; i <= dy; i++) {
+        board[i][x] = current_color;
+      }
+    } else if (x === dx && y < dy) {
+      // 下から上の場合
+      // eslint-disable-next-line for-direction
+      for (let i = y; i >= dy; i--) {
+        board[i][x] = current_color;
+      }
+    } else if (y === dy && x > dx) {
+      //左から右の場合
+      // eslint-disable-next-line for-direction
+      for (let i = x; i <= dx; i++) {
+        board[y][i] = current_color;
+      }
+    } else if (y === dy && x < dx) {
+      //右から左の場合
+      // eslint-disable-next-line for-direction
+      for (let i = x; i >= dx; i--) {
+        board[y][i] = current_color;
+      }
+    }
+  }
+
+  return board;
+}
+
 function bottomToUp(x: number, y: number, c: number, board: number[][]): [number, number, boolean] {
   board[y][x] = c;
   let canput = true;
@@ -105,8 +139,8 @@ function rightToLeft(
   const differentColor = c === 1 ? 2 : 1;
   let last = 0;
   // eslint-disable-next-line for-direction
-  for (let i: number = x; i >= 0; i--) {
-    const current = board[i][y];
+  for (let i: number = x - 1; i >= 0; i--) {
+    const current = board[y][i];
     if (current === differentColor) {
       differentColorIsNext = true;
     } else if (current === c) {
@@ -124,21 +158,27 @@ function rightToLeft(
   }
 }
 
-function canPut(x: number, y: number, c: number, board: number[][]): boolean {
+function canPut(x: number, y: number, c: number, board: number[][]): [boolean, number[][]] {
   let ans = false;
-  const result1 = bottomToUp(x, y, c, structuredClone(board))[2];
-  const result2 = upToBottom(x, y, c, structuredClone(board))[2];
-  const result3 = leftToRight(x, y, c, structuredClone(board))[2];
-  const result4 = rightToLeft(x, y, c, structuredClone(board))[2];
-  if (result1 || result2 || result3 || result4) {
+  const result = [];
+  const result1 = bottomToUp(x, y, c, structuredClone(board));
+  const result2 = upToBottom(x, y, c, structuredClone(board));
+  const result3 = leftToRight(x, y, c, structuredClone(board));
+  const result4 = rightToLeft(x, y, c, structuredClone(board));
+  if (result1[2] || result2[2] || result3[2] || result4[2]) {
     ans = true;
   }
-  console.log('1:', result1);
-  console.log('2:', result2);
-  console.log('3:', result3);
-  console.log('4:', result4);
+  result.push([result1[0], result1[1]]);
+  result.push([result2[0], result2[1]]);
+  result.push([result3[0], result3[1]]);
+  result.push([result4[0], result4[1]]);
+
+  console.log('1:', result1[2]);
+  console.log('2:', result2[2]);
+  console.log('3:', result3[2]);
+  console.log('4:', result4[2]);
   console.log('done');
-  return ans;
+  return [ans, result];
 }
 
 const Home = () => {
@@ -158,12 +198,15 @@ const Home = () => {
   const [player, setPlayer] = useState<number>(1); // 1 is white, 2 is black
 
   const clickCell = (x: number, y: number) => {
-    const board_copy = structuredClone(board);
+    let board_copy = structuredClone(board);
     console.log('click');
     if (board_copy[y][x] === 0) {
-      if (canPut(x, y, player, structuredClone(board_copy))) {
+      const can = canPut(x, y, player, structuredClone(board_copy));
+      if (can[0]) {
         //copy board
         board_copy[y][x] = player;
+
+        board_copy = update_board(x, y, can[1], board_copy);
 
         setBoard(board_copy);
         // run if click
