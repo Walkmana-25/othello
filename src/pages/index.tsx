69a318342de1690ff833cd7e1cfbf6b98bcc44b2
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-15 16:25:35 +0900

    Update user authentication logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,304 +21,6 @@ const discColor = (num: number): string => {
   }
 };
 
-function updateBoard(x: number, y: number, range: number[][], board: number[][]): number[][] {
-  const current_color = board[y][x];
-
-  for (const [dx, dy] of range) {
-    if (dx === -1 || dy === -1) continue;
-    //上から下の場合
-    else if (x === dx && y > dy) {
-      console.log('上から下の場合');
-      for (let i = y; i >= dy; i--) {
-        board[i][x] = current_color;
-      }
-    } else if (x === dx && y < dy) {
-      // 下から上の場合
-      console.log('下から上の場合');
-      for (let i = y; i <= dy; i++) {
-        board[i][x] = current_color;
-      }
-    } else if (y === dy && x < dx) {
-      //左から右の場合
-      console.log('左から右の場合');
-      for (let i = x; i <= dx; i++) {
-        board[y][i] = current_color;
-      }
-    } else if (y === dy && x > dx) {
-      //右から左の場合
-      console.log('右から左の場合');
-      for (let i = x; i >= dx; i--) {
-        board[y][i] = current_color;
-      }
-    } else if (x < dx && y < dy) {
-      //左下から右上の場合
-      console.log('左下から右上の場合');
-      let j = y;
-      for (let i = x; i <= dx; i++) {
-        board[j][i] = current_color;
-        j++;
-        if (i === dx || j === dy) break;
-      }
-    } else if (x > dx && y > dy) {
-      //右上から左下の場合
-      console.log('右上から左下の場合');
-      let j = y;
-      for (let i = x; i >= dx; i--) {
-        board[j][i] = current_color;
-        j--;
-        if (i === dx || j === dy) break;
-      }
-    } else if (x > dx && y < dy) {
-      //左上から右下の場合
-      console.log('左上から右下の場合');
-      let j = y;
-      for (let i = x; i >= dx; i--) {
-        board[j][i] = current_color;
-        j++;
-        if (i === dx || j === dy) break;
-      }
-    } else if (x < dx && y > dy) {
-      //右下から左上の場合
-      console.log('右下から左上の場合');
-      let j = y;
-      for (let i = x; i <= dx; i++) {
-        board[j][i] = current_color;
-        j--;
-        if (i === dx || j === dy) break;
-      }
-    }
-  }
-
-  return board;
-}
-
-function bottomToUp(x: number, y: number, c: number, board: number[][]): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  for (let i: number = y - 1; i >= 0; i--) {
-    const current = board[i][x];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [last, i, true];
-    } else {
-      return [-1, -1, false];
-    }
-  }
-
-  return [-1, -1, false];
-}
-
-function upToBottom(x: number, y: number, c: number, board: number[][]): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  for (let i: number = y + 1; i < 8; i++) {
-    const current = board[i][x];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [x, last, true];
-    } else {
-      return [-1, -1, false];
-    }
-  }
-
-  return [-1, -1, false];
-}
-function leftToRight(
-  x: number,
-  y: number,
-  c: number,
-  board: number[][],
-): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  for (let i: number = x + 1; i < 8; i++) {
-    const current = board[y][i];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [last, y, true];
-    } else {
-      return [-1, -1, false];
-    }
-  }
-
-  return [-1, -1, false];
-}
-
-function rightToLeft(
-  x: number,
-  y: number,
-  c: number,
-  board: number[][],
-): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  for (let i: number = x - 1; i >= 0; i--) {
-    const current = board[y][i];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [last, y, true];
-    } else {
-      return [-1, -1, false];
-    }
-  }
-
-  return [-1, -1, false];
-}
-
-function leftBottomToRightUp(
-  x: number,
-  y: number,
-  c: number,
-  board: number[][],
-): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  let j = y - 1;
-  for (let i: number = x + 1; i < 8; i++) {
-    if (i < 0 || j < 0 || 7 < i || 7 < j) return [-1, -1, false];
-    const current = board[j][i];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [last, j, true];
-    } else {
-      return [-1, -1, false];
-    }
-    j--;
-  }
-
-  return [-1, -1, false];
-}
-
-function rightUpToLeftBottom(
-  x: number,
-  y: number,
-  c: number,
-  board: number[][],
-): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  let j = y + 1;
-  for (let i: number = x - 1; i >= 0; i--) {
-    if (i < 0 || j < 0 || 7 < i || 7 < j) return [-1, -1, false];
-    const current = board[j][i];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [last, j, true];
-    } else {
-      return [-1, -1, false];
-    }
-    j++;
-  }
-
-  return [-1, -1, false];
-}
-
-function rightBottomToLeftUp(
-  x: number,
-  y: number,
-  c: number,
-  board: number[][],
-): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  let j = y - 1;
-  for (let i: number = x - 1; i >= 0; i--) {
-    if (i < 0 || j < 0 || 7 < i || 7 < j) return [-1, -1, false];
-
-    const current = board[j][i];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [last, j, true];
-    } else {
-      return [-1, -1, false];
-    }
-    j--;
-  }
-  return [-1, -1, false];
-}
-
-function leftUpToRightBottom(
-  x: number,
-  y: number,
-  c: number,
-  board: number[][],
-): [number, number, boolean] {
-  board[y][x] = c;
-  let differentColorIsNext = false;
-  const differentColor = c === 1 ? 2 : 1;
-  let last = 0;
-  let j = y + 1;
-  for (let i: number = x + 1; i < 8; i++) {
-    if (i < 0 || j < 0 || 7 < i || 7 < j) return [-1, -1, false];
-
-    const current = board[j][i];
-    if (current === 3 || current === 0) {
-      return [-1, -1, false];
-    }
-
-    if (current === differentColor) {
-      differentColorIsNext = true;
-    } else if (current === c && differentColorIsNext === true) {
-      last = i;
-      return [last, j, true];
-    } else {
-      return [-1, -1, false];
-    }
-    j++;
-  }
-
-  return [-1, -1, false];
-}
-
 function canPutWithPlace(
   x: number,
   y: number,
@@ -326,48 +28,6 @@ function canPutWithPlace(
   board: number[][],
 ): [boolean, number[][]] {
   let ans = false;
-  const result = [];
-  const result1 = bottomToUp(x, y, c, structuredClone(board));
-  const result2 = upToBottom(x, y, c, structuredClone(board));
-  const result3 = leftToRight(x, y, c, structuredClone(board));
-  const result4 = rightToLeft(x, y, c, structuredClone(board));
-  const result5 = leftBottomToRightUp(x, y, c, structuredClone(board));
-  const result6 = rightUpToLeftBottom(x, y, c, structuredClone(board));
-  const result7 = rightBottomToLeftUp(x, y, c, structuredClone(board));
-  const result8 = leftUpToRightBottom(x, y, c, structuredClone(board));
-
-  if (
-    result1[2] ||
-    result2[2] ||
-    result3[2] ||
-    result4[2] ||
-    result5[2] ||
-    result6[2] ||
-    result7[2] ||
-    result8[2]
-  ) {
-    ans = true;
-  }
-  result.push([result1[0], result1[1]]);
-  result.push([result2[0], result2[1]]);
-  result.push([result3[0], result3[1]]);
-  result.push([result4[0], result4[1]]);
-  result.push([result5[0], result5[1]]);
-  result.push([result6[0], result6[1]]);
-  result.push([result7[0], result7[1]]);
-  result.push([result8[0], result8[1]]);
-
-  console.log('1:', result1[2]);
-  console.log('2:', result2[2]);
-  console.log('3:', result3[2]);
-  console.log('4:', result4[2]);
-  console.log('5:', result5[2]);
-  console.log('6:', result6[2]);
-  console.log('7:', result7[2]);
-  console.log('8:', result8[2]);
-
-  console.log('done');
-
   const directions: number[][] = [
     [-1, -1],
     [-1, 0],
@@ -378,17 +38,40 @@ function canPutWithPlace(
     [1, 0],
     [1, 1],
   ];
+  const boardCopy = structuredClone(board);
+  const changeBoard: number[][] = [];
   for (const direction of directions) {
+    const changeBoardTemp: number[][] = [];
     const dx = direction[0];
     const dy = direction[1];
+
     console.log(dx, dy);
+
+    let differentColorIsNext = false;
     for (let i: number = 1; i < 8; i++) {
       const cx: number = x + dx * i;
       const cy: number = y + dy * i;
+      if (cx < 0 || cy < 0 || cx >= 8 || cy >= 8) continue;
+      const current: number = boardCopy[cx][cy];
+
+      if (current === 0) {
+        break;
+      } else if (i === 1 && current === c) {
+        changeBoardTemp.push([cx, cy]);
+        continue;
+      } else if (current === (c === 1 ? 2 : 1)) {
+        changeBoardTemp.push([cx, cy]);
+        differentColorIsNext = true;
+      } else if (differentColorIsNext && current === c) {
+        changeBoardTemp.push([cx, cy]);
+        changeBoard.push(...changeBoardTemp);
+        ans = true;
+        break;
+      }
     }
   }
 
-  return [ans, result];
+  return [ans, changeBoard];
 }
 
 const Home = () => {
@@ -455,7 +138,7 @@ const Home = () => {
 
   const clickCell = (x: number, y: number, p: number, s: string, board: number[][]) => {
     console.log('click');
-    let board_copy = structuredClone(board);
+    const board_copy = structuredClone(board);
 
     const current = structuredClone(board_copy[y][x]);
 
@@ -468,7 +151,9 @@ const Home = () => {
         //copy board
         board_copy[y][x] = p;
 
-        board_copy = updateBoard(x, y, can[1], board_copy);
+        can[1].map((place) => {
+          board_copy[place[0]][place[1]] = p;
+        });
 
         //change turn
         if (current === 0) {
